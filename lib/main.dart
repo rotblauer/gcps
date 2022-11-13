cb5108a6ee467db8d82fedba46ed4ce03672d57e
meows <b5c6@protonmail.com>
2022-11-13 09:37:01 -0800

    lib: bump camera resolution to MAX (from ultraHigh)

--- lib/main.dart
+++ lib/main.dart
@@ -2588,7 +2588,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       // Get a specific camera from the list of available cameras.
       widget.camera,
       // Define the resolution to use.
-      ResolutionPreset.ultraHigh,
+      ResolutionPreset.max,
       enableAudio: false,
     );
 
